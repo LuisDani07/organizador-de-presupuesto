5a30b6c75c6bb3a0142cbb9f4193b68cbd935d4b
LuisDani07 <tics270118@gmail.com>
2022-11-13 11:41:45 -0700

    el sobrante del presupuesto menos los gastos

--- src/components/Remaining.jsx
+++ src/components/Remaining.jsx
@@ -1,9 +1,14 @@
-
+import {AppContext} from '../context/AppContext'
+import {useContext} from 'react'
 
 function Remaining() {
+  const {expenses, budget}=useContext(AppContext);
+  const totalExpenses=expenses.reduce((total, item)=>{
+    return total=total+item.cost;
+  },0)
   return (
       <div className="alert alert-success">
-        <span>Remaining: $1200</span>
+        <span>Remaining: ${budget-totalExpenses}</span>
       </div>
   )
 }
