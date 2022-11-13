abf06f1a9ac6572b418bd6ac1391b4b2978876ca
LuisDani07 <tics270118@gmail.com>
2022-11-13 11:53:46 -0700

    todos los gastos totales

--- src/components/ExpenseTotal.jsx
+++ src/components/ExpenseTotal.jsx
@@ -1,9 +1,16 @@
 
-
+import { useContext } from 'react'
+import {AppContext} from '../context/AppContext'
 function ExpenseTotal() {
+  const {expenses}=useContext(AppContext);
+  const totalExpenses=expenses.reduce((total, item)=>{
+    return (total+=item.cost);
+  },0);
+
+
   return (
     <div className="alert alert-primary">
-        <span>spent so far: $1200</span>
+        <span>spent so far: ${totalExpenses}</span>
     </div>
   )
 }
