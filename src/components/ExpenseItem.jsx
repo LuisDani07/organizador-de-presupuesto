e37723fdc23d9ffe8d6d3436fab26c5d60200967
LuisDani07 <tics270118@gmail.com>
2022-11-13 12:12:12 -0700

    función de eliminar gastos

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,6 +1,15 @@
-
+import { useContext } from 'react';
+import {AppContext} from '../context/AppContext';
 
 function ExpenseItem({id, name, cost}) {
+  const {dispatch}=useContext(AppContext);
+
+  const handleDeleteExpense=()=>{
+    dispatch({
+      type:'DELETE_EXPENSE',
+      payload:id
+    })
+  }
   return (
     <li className="list-group-item d-flex justify-content-between align-items-center">
         {name}
@@ -8,7 +17,7 @@ function ExpenseItem({id, name, cost}) {
             <span className="badge badge-primary badge-pill mr-3 bg-success">
                 ${cost}
             </span>
-            <button className="btn btn-danger">Delete</button>
+            <button onClick={handleDeleteExpense} className="btn btn-danger">Delete</button>
         </div>
     </li>
   )
