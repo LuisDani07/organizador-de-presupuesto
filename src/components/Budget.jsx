23faf38c5fd40733503de1822c4ecc244bdaa50d
LuisDani07 <tics270118@gmail.com>
2022-11-13 18:07:49 -0700

    errores corregidos

--- src/components/Budget.jsx
+++ src/components/Budget.jsx
@@ -3,9 +3,11 @@ import {AppContext} from '../context/AppContext';
 
 function Budget() {
   const {budget}=useContext(AppContext)
+
   return (
-    <div className="alert alert-secondary">
+    <div className="alert alert-secondary d-flex">
         <span>Budget: $ {budget}</span>
+        <button  className='btn btn-primary ms-5'>Edit</button>
     </div>
   )
 }
